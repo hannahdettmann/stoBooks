f006211126efba8263664642c35adcd0df3006f0
HSkalski <harrison.skalski@gmail.com>
2019-07-11 20:35:53 -0500

    testing material ui

--- src/App.js
+++ src/App.js
@@ -1,23 +1,18 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Button from '@material-ui/core/Button';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        <Button variant="contained" color="primary">
+          I'm a button!
+        </Button>
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Testing React
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
       </header>
     </div>
   );
